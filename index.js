ba6630a75a47cd1ea9947d35714732f395458376
Derek Holland <derekhollandwebdev@gmail.com>
2023-07-04 14:48:06 -0400

    project complete

--- index.js
+++ index.js
@@ -14,25 +14,43 @@ let mass = document.getElementById('mass')
 // add event listener to button
 convertBtn.addEventListener('click', function () {
 	convertLength(userInput.value)
+	convertVolume(userInput.value)
 	convertMass(userInput.value)
 })
 
 function convertLength(num) {
-	// Meters = Feet
-	let meter = 3.281 //feet per meter
-	let metersToFeet = meter * num
-	let baseString1 = `${num} meters = ${metersToFeet.toFixed(3)} feet`
-	// Feet = Meters
-	let feet = 0.3048 //meters per foot
-	let feetToMeters = feet * 20
-	let baseString2 = `${num} feet = ${feetToMeters.toFixed(3)} meters`
-	// Send data to render function to display results on the page
-	render(baseString1, baseString2)
+	// Feet per 1 meter
+	let feetPerMeter = 3.281
+	let totalFeet = feetPerMeter * num
+	let baseString = `${num} meters = ${totalFeet.toFixed(3)} feet`
+	// Meters per 1 foot
+	let metersPerFoot = 0.3048 //meters per foot
+	let totalMeters = metersPerFoot * 20
+	baseString += ` | ${num} feet = ${totalMeters.toFixed(3)} meters`
+	// render string to the DOM
+	length.textContent = baseString
 }
-function convertMass(num) {}
-function convertVolume(num) {}
-
-function render(string1, string2) {
-	console.log(string1)
-	console.log(string2)
+function convertVolume(num) {
+	// Gallons per 1 liter
+	let gallonsPerLiter = 0.264 //gallons per liter
+	let totalGallons = gallonsPerLiter * num
+	let baseString = `${num} liters = ${totalGallons.toFixed(3)} gallons`
+	// Liters per 1 gallon
+	let litersPerGallon = 3.785
+	let totalLiters = litersPerGallon * num
+	baseString += ` | ${num} gallons = ${totalLiters.toFixed(3)} liters`
+	// render string to the DOM
+	volume.textContent = baseString
+}
+function convertMass(num) {
+	// Pounds per 1 kilo
+	let poundsPerKilo = 2.204
+	let totalPounds = poundsPerKilo * num
+	let baseString = `${num} kilos = ${totalPounds.toFixed(3)} pounds`
+	// Kilos per 1 pound
+	let kilosPerPound = 0.453
+	let totalKilos = num * kilosPerPound
+	baseString += ` | ${num} pounds = ${totalKilos.toFixed(3)} kilos`
+	// render string to the DOM
+	mass.textContent = baseString
 }
